9057663bfc30080bf77209054ba7a57ce2ba8f0e
omayma ahmad <omayma.ahmad25@gmail.com>
2021-02-22 00:36:50 +0000

    feat: added controllers file

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -3,4 +3,26 @@ var burger = require("../models/burger");
 
 var router = express.Router();
 
+router.get("/", function (req, res) {
+    burger.all(function (data) {
+        const hbsObject = {
+            burger: data,
+        };
+        res.render("index", hbsObject);
+    });
+});
+
+router.post("/api/burger", (req, res) => {
+    burger.create(req.body.burger_name, function () {
+        res.redirect("/");
+    });
+});
+
+router.put("/api/buger/:id", (req, res) => {
+    burger.update(req.params.id, function(result) {
+        console.log(result);
+        res.sendStatus(200);
+    });
+});
+
 module.exports = router; 
