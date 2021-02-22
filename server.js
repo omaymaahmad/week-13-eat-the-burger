61982fa4fb141a6cbdd721d857f8bb9c15db0c9b
omayma ahmad <omayma.ahmad25@gmail.com>
2021-02-22 00:30:27 +0000

    feat: completed script file

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
 const express = require("express"); 
+const routes = require("./controllers/burgers_controllers");
+const expressHb = require("express-handlebars");
+const PORT = process.env || 3000;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(__dirname));
+app.use(routes);
+app.engine("handlebars", expressHb({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+
+app.listen(PORT, () => console.log("app is running on" + PORT));
