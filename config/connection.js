4fefa83f07101c4cf90daf4151cfdce526e429c1
omayma ahmad <omayma.ahmad25@gmail.com>
2021-03-07 15:35:21 +0000

    feat: final css and styling

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,7 @@ if(process.env.JAWSDB_URL){
         host: "localhost",
         port: 3306,
         user: "root",
-        password: "Jabeen98",
+        password: "",
         database: "burgers_db"
     })}
 
